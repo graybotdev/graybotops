d5d0e645ecb31891b23c641469ce704862ed97b0
Chris <graybot@graybotops.com>
2025-04-25 17:23:55 -0500

    Fix: switch Google OAuth to run_console for cloud deploys

--- app/email_reader.py
+++ app/email_reader.py
@@ -41,7 +41,8 @@ def get_service():
                 tmp.flush()
                 flow = InstalledAppFlow.from_client_secrets_file(tmp.name, SCOPES)
 
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
+
 
         with open(TOKEN_PICKLE, 'wb') as token:
             pickle.dump(creds, token)
